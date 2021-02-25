e03d26ea9278c9467990b323e56dd9d216aa1764
PaymasterMax <duncansantiago18@gmail.com>
2021-02-25 14:17:24 -0700

    patch auto-gen by /usr/local/sbin/gm under account punk

---
+++ Asserts/static/js/home.js
@@ -0,0 +1,3 @@
+document.querySelector("body").addEventListener("load",(e)=>{
+  alert("Done")
+})
--- Home/views.py
+++ Home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homeView(request):
-    return HttpResponse("home")
+    return render(request,"Home/home.html",context = {
+    })
