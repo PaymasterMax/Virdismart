661bc1c7b3c0b7481f897984f4ad676aea8f34dd
PaymasterMax <duncansantiago18@gmail.com>
2021-03-12 17:30:39 -0700

    patch auto-gen by /usr/local/sbin/gm under account punk

--- Home/static/home/js/home.js
+++ Home/static/home/js/home.js
@@ -6,10 +6,10 @@ $(".stats").click((e)=>{
   storage.setItem("redirect","/blogs/")
   window.location = "/stats/"
 })
-// window.addEventListener("scroll", function (e) {
-//     let currentPosition = window.pageYOffset;
-//     console.log(currentPosition);
-//     if (elHeight==(currentPosition + screenHeight)) {
-//         console.log("Here I am");
-//     }
-// })
+window.addEventListener("scroll", function (e) {
+    let currentPosition = window.pageYOffset;
+    console.log(currentPosition);
+    if (elHeight==(currentPosition + screenHeight)) {
+        console.log("Here I am");
+    }
+})
